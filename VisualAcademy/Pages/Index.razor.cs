fe4fc44f6c853aa0127882574583c49e8ce77a0a
Ray <dlghdnd@gmail.com>
2022-11-30 01:40:06 +0900

    carousel 기능 추가했는데 사용은 안함

---
+++ VisualAcademy/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace VisualAcademy.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool forstRender)
+        {
+            await JSRuntime.InvokeAsync<object>("RunCarousel");
+        }
+    }
+}
